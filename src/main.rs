4074b2b503647c8bd56e712f66508f2bf198c5b9
Antonio Yang <yanganto@gmail.com>
2020-04-17 23:29:55 +0800

    change source

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,7 @@ use libc::c_char;
 use regex::Regex;
 use reqwest::header;
 use std::ffi::CStr;
-use std::str;
-use std::u64;
+use std::{env, str, u64};
 
 #[link(name = "ethash_proof")]
 extern "C" {
@@ -19,17 +18,28 @@ extern "C" {
 async fn adaptor(body: String) -> impl Responder {
     println!("req: {}", body);
     let para = Regex::new(r#"params":\s?\["0x([^"]+?)",\s?\w+\]"#).unwrap();
-    let block_number_hex_str = para.captures_iter(&body).nth(0).map(|c| c[1].to_string());
+    let block_number_hex_str = para
+        .captures_iter(&body)
+        .nth(0)
+        .map(|c| c[1].to_string())
+        .unwrap();
     println!("block number: {:#?}", block_number_hex_str);
-    let block_number = u64::from_str_radix(&block_number_hex_str.unwrap(), 16).unwrap();
-    println!("block number: {}", block_number);
-    let client = reqwest::Client::new();
-
-    let req = client
-        .post("https://cloudflare-eth.com/")
-        .header(header::CONTENT_TYPE, "application/json")
-        .body(body)
-        .send();
+    let block_number = u64::from_str_radix(&block_number_hex_str, 16).unwrap();
+    let req = if let Ok(api_key) = env::var("ETHSCANAPIKEY") {
+        println!("api key: {}", api_key);
+        let client = reqwest::Client::new();
+        client
+            .get(&format!(
+			"https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=0x{}&boolean=false&apikey={}", block_number_hex_str, api_key)).send()
+    } else {
+        println!("block number: {}", block_number);
+        let client = reqwest::Client::new();
+        client
+            .post("https://cloudflare-eth.com/")
+            .header(header::CONTENT_TYPE, "application/json")
+            .body(body)
+            .send()
+    };
     let output: *const c_char = unsafe { EthashProof(block_number) };
     let c_str: &CStr = unsafe { CStr::from_ptr(output) };
     let str_slice: &str = c_str.to_str().unwrap();
